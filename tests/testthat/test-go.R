140e56d41d1ca66165aed9e51a5160a360451fdf
Charlotte Dawson <csdaw@outlook.com>
2020-08-03 16:03:09 +0100

    Add small test for get_ancestor_go
    Related to #1.

--- tests/testthat/test-go.R
+++ tests/testthat/test-go.R
@@ -2,4 +2,20 @@ context("go")
 
 #### Setup ---------------------------------------------------------------------
 
+# RNA binding and tRNA binding GO terms (both biological process)
+rbp.go <- c("GO:0003723", "GO:0000049")
+
+# Make a small GO table of the expected format
+go.df <- data.frame(
+  UNIPROTKB = c("Q9BYJ9", "Q9NWX6"),
+  GO.ID = rbp.go
+)
+
 #### Tests ---------------------------------------------------------------------
+
+test_that("get_ancestor_go works", {
+  ancestor.terms <- get_ancestor_go(go.df, verbose = FALSE)
+
+  expect_equal(nrow(ancestor.terms), 13)
+  expect_false(any(is.na(ancestor.terms)))
+})
