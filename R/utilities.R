d0c57373e50a103d8e81ac3133179365769dba61
Charlotte Dawson <csdaw@outlook.com>
2020-08-03 12:26:42 +0100

    Add remove_dots utility function

---
+++ R/utilities.R
@@ -0,0 +1,21 @@
+#' Remove duplicated full stops
+#'
+#' @description A convenience function to remove any duplicated full stops
+#' (aka periods) from the elements of a vector, e.g. will change `..` or `...`
+#' or `....` etc. to just `.` Mainly used to fix column names.
+#'
+#' @param x `character`. Contains duplicate full stops to be removed.
+#'
+#' @return Returns `character` with duplicate full stops removed.
+#' @examples
+#'
+#' df <- data.frame(
+#'   column...name = c(1, 2, 3)
+#' )
+#'
+#' colnames(df) <- remove_dots(colnames(df))
+#'
+#' @export
+remove_dots <- function(x) {
+  gsub("(?<=\\.)\\.+", "", x, perl = TRUE)
+}
---
+++ tests/testthat/test-utilities.R
@@ -0,0 +1,14 @@
+context("utilites")
+
+#### Setup ---------------------------------------------------------------------
+
+#### Tests ---------------------------------------------------------------------
+
+test_that("remove_dots works", {
+  df <- data.frame(
+    column...name = c(1, 2, 3)
+  )
+
+  colnames(df) <- remove_dots(colnames(df))
+  expect_equal(grep("\\.", colnames(df)), 1)
+})
