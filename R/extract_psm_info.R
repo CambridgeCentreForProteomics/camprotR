38ce0ed1fad334fc6e616060015d3b3c88866d36
TomSmithCGAT <tss38@cam.ac.uk>
2020-09-15 10:51:30 +0100

    adds regression tests for PSM filtering functions

--- R/extract_psm_info.R
+++ R/extract_psm_info.R
@@ -1,7 +1,7 @@
 #' Extract peptide sequencing and interference information from SILAC
 #' PSM-level output
 #'
-#' @description Proteome Discoverer does not correctly propogate from PSM to
+#' @description Proteome Discoverer does not correctly propagate from PSM to
 #' peptide level output which intensities are from "sequenced" peptides, e.g MS2
 #' fragmentation. This information is useful to assess the accuracy of
 #' quantification in peptides identified by mass shift relative to a sequenced
@@ -9,29 +9,37 @@
 #'
 #' @param obj `data.frame` PSM-level output from Proteome Discoverer
 #' @param sequence_col `string` Column with peptide sequence
+#' @param mod_col `string` Column with modifications
+#' @param interference_col `string` Column with interference/co-isolation
 #' @return `data.frame` indicating which SILAC peptides were MS2 sequenced and
 #' the maximum interference across all PSMs for the peptide
 #' @export
-silac_psm_seq_int <- function(obj, sequence_col='Sequence'){
+silac_psm_seq_int <- function(
+  obj,
+  sequence_col='Sequence',
+  mod_col='Modifications',
+  interference_col='Isolation.Interference.in.Percent'){
 
   obj <- obj %>%
     filter(.data$Quan.Channel!='') %>%
     rowwise() %>%
     filter(is.finite(.data$Precursor.Abundance)) %>%
-    remove_silac_modifications(level='psm') %>%
-    mutate(Modifications=psm_to_peptide_style_modifications(.data$Modifications))
+    # mutate(mod_col=remove_silac_modifications(!!sym(mod_col), level='psm')) %>%
+    mutate(mod_col=psm_to_peptide_style_modifications(!!sym(mod_col)))
 
   obj_seq <- obj %>%
-    group_by(!!sym(sequence_col), .data$Quan.Channel, .data$Modifications) %>%
+    group_by(!!sym(sequence_col), Quan.Channel, !!sym(mod_col)) %>%
     summarise(sequenced=any(is.finite(.data$Precursor.Abundance))) %>%
     mutate(Quan.Channel=paste0('Sequenced_', .data$Quan.Channel)) %>%
-    spread(key=.data$Quan.Channel, value=.data$sequenced, fill=FALSE)
+    spread(key=Quan.Channel, value=sequenced, fill=FALSE)
 
   obj_int <- obj %>%
-    group_by(!!sym(sequence_col), .data$Modifications) %>%
-    summarise(max_interference=max(.data$Isolation.Interference.in.Percent))
+    group_by(!!sym(sequence_col), !!sym(mod_col)) %>%
+    summarise(max_interference=max(!!sym(interference_col)))
 
   obj_seq_int<- merge(obj_seq, obj_int, by=c(sequence_col, 'Modifications'))
 
   return(obj_seq_int)
 }
+
+
---
+++ tests/testthat/test-extact_psm_info.R
@@ -0,0 +1,7 @@
+context('Extracting sequenced and interference data from PSM')
+
+test_that("Extract info", {
+  data('silac_psm_data', package='camprotR')
+  expect_equal_to_reference(
+    silac_psm_seq_int(silac_psm_data, sequence_col='Annotated.Sequence'), file='psm_sequenced.rds')
+})
