eec73c3e97d4380ddf45ce6184ee0103aab9c1b3
TomSmithCGAT <tss38@cam.ac.uk>
2020-09-21 20:30:08 +0100

    removed silac_psm_seq_int test output sort

--- tests/testthat/test-extact_psm_info.R
+++ tests/testthat/test-extact_psm_info.R
@@ -3,6 +3,5 @@ context('Extracting sequenced and interference data from PSM')
 test_that("Extract info", {
   data('silac_psm_data', package='camprotR')
   expect_equal_to_reference(
-    arrange(silac_psm_seq_int(silac_psm_data, sequence_col='Annotated.Sequence'), Annotated.Sequence),
-    file='psm_sequenced.rds')
+    silac_psm_seq_int(silac_psm_data, sequence_col='Annotated.Sequence'), file='psm_sequenced.rds')
 })
