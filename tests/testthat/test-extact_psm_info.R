6e253cb10a20b1738fc4c450977d39131db5a6dc
TomSmithCGAT <tss38@cam.ac.uk>
2020-09-25 15:29:28 +0100

    replaces expect_equal_to_reference with expect_equal for extract_psm_info test

--- tests/testthat/test-extact_psm_info.R
+++ tests/testthat/test-extact_psm_info.R
@@ -2,18 +2,11 @@ context('Extracting sequenced and interference data from PSM')
 
 test_that("Extract info", {
   data('silac_psm_data', package='camprotR')
-  output <-  silac_psm_seq_int(silac_psm_data, sequence_col='Annotated.Sequence')
+  out <-  silac_psm_seq_int(silac_psm_data, sequence_col='Annotated.Sequence')
 
-  ref_file <- 'psm_sequenced.rds'
-  print(file.exists(ref_file))
-  ref <- readRDS(ref_file)
-  print(nrow(ref))
-  print(nrow(dplyr::arrange(output, Annotated.Sequence, Modifications)))
-  print(dplyr::arrange(ref, Annotated.Sequence, Modifications)[1,])
-  print(dplyr::arrange(output, Annotated.Sequence, Modifications)[1,])
-  stop()
-
-  expect_equal_to_reference(
-    dplyr::arrange(output, Annotated.Sequence, Modifications), # sort order appears to be env-specific
-    file='psm_sequenced.rds')
+  #expect_equal_to_reference(
+  #  dplyr::arrange(output, Annotated.Sequence, Modifications), # sort order appears to be env-specific
+  #  file='psm_sequenced.rds')
+  ref <- readRDS('psm_sequenced.rds') %>% dplyr::arrange(Annotated.Sequence, Modifications)
+  expect_equal(out, ref)
 })
