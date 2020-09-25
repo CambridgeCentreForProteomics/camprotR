094bc5e80dc264fe217069bc30039d57e1153dae
TomSmithCGAT <tss38@cam.ac.uk>
2020-09-25 15:45:01 +0100

    debug expect_equal

--- tests/testthat/test-extact_psm_info.R
+++ tests/testthat/test-extact_psm_info.R
@@ -2,11 +2,13 @@ context('Extracting sequenced and interference data from PSM')
 
 test_that("Extract info", {
   data('silac_psm_data', package='camprotR')
-  out <-  silac_psm_seq_int(silac_psm_data, sequence_col='Annotated.Sequence')
+  out <-  silac_psm_seq_int(silac_psm_data, sequence_col='Annotated.Sequence') %>%
+    dplyr::arrange(Annotated.Sequence, Modifications)
 
   #expect_equal_to_reference(
   #  dplyr::arrange(output, Annotated.Sequence, Modifications), # sort order appears to be env-specific
   #  file='psm_sequenced.rds')
-  ref <- readRDS('psm_sequenced.rds') %>% dplyr::arrange(Annotated.Sequence, Modifications)
+  ref <- readRDS('psm_sequenced.rds') %>%
+    dplyr::arrange(Annotated.Sequence, Modifications)
   expect_equal(out, ref)
 })
