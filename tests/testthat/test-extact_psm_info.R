fe453454cfbe0612695b0b97213c4f782ad9751a
Tom Smith <tss38@cam.ac.uk>
2020-09-25 15:56:28 +0100

    {ts} add psm filter reg tests sb1 (#10)
    * reverts to default travis script commands
    
    * removes dummy test function
    
    * replaces expect_equal_to_reference with expect_equal for extract_psm_info test

--- tests/testthat/test-extact_psm_info.R
+++ tests/testthat/test-extact_psm_info.R
@@ -2,6 +2,13 @@ context('Extracting sequenced and interference data from PSM')
 
 test_that("Extract info", {
   data('silac_psm_data', package='camprotR')
-  expect_equal_to_reference(
-    silac_psm_seq_int(silac_psm_data, sequence_col='Annotated.Sequence'), file='psm_sequenced.rds')
+  out <-  silac_psm_seq_int(silac_psm_data, sequence_col='Annotated.Sequence') %>%
+    dplyr::arrange(Annotated.Sequence, Modifications)
+
+  #expect_equal_to_reference(
+  #  dplyr::arrange(output, Annotated.Sequence, Modifications), # sort order appears to be env-specific
+  #  file='psm_sequenced.rds')
+  ref <- readRDS('psm_sequenced.rds') %>%
+    dplyr::arrange(Annotated.Sequence, Modifications)
+  expect_equal(out, ref)
 })
