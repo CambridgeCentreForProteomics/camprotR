d1270f56f8059e8d427982a5d36bf037198431ee
TomSmithCGAT <tss38@cam.ac.uk>
2020-09-25 14:57:10 +0100

    adds print statement to debug travis fail

--- tests/testthat/test-extact_psm_info.R
+++ tests/testthat/test-extact_psm_info.R
@@ -4,6 +4,15 @@ test_that("Extract info", {
   data('silac_psm_data', package='camprotR')
   output <-  silac_psm_seq_int(silac_psm_data, sequence_col='Annotated.Sequence')
 
+  ref_file <- 'psm_sequenced.rds'
+  print(file.exists(ref_file))
+  ref <- readRDS(ref_file)
+  print(nrow(ref))
+  print(nrow(dplyr::arrange(output, Annotated.Sequence, Modifications)))
+  print(ref[1,])
+  print(dplyr::arrange(output, Annotated.Sequence, Modifications)[1,])
+  stop()
+
   expect_equal_to_reference(
     dplyr::arrange(output, Annotated.Sequence, Modifications), # sort order appears to be env-specific
     file='psm_sequenced.rds')
