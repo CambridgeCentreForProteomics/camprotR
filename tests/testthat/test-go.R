ac415778d07d57d50f485cb9a3e1ebe37a28b83c
Charlotte Dawson <csdaw@outlook.com>
2020-10-30 16:04:43 +0000

    Add tests for GO functions

--- tests/testthat/test-go.R
+++ tests/testthat/test-go.R
@@ -13,6 +13,43 @@ go.df <- data.frame(
 
 #### Tests ---------------------------------------------------------------------
 
+test_that("determine_ancestor_function works", {
+  go.term <- rbp.go[1]
+
+  go.relations <- AnnotationDbi::get(go.term, determine_ancestor_function(go.term, "MF"))
+  expect_equal(length(go.relations), 6)
+  expect_equal(go.relations[6], "all")
+})
+
+test_that("determine_offspring_function works", {
+  go.term <- rbp.go[1]
+
+  go.relations <- AnnotationDbi::get(go.term, determine_offspring_function(go.term, "MF"))
+  expect_equal(length(go.relations), 173)
+})
+
+test_that("get_all_mappings works", {
+  rbp.ontologies <- c("MF", "MF")
+  names(rbp.ontologies) <- rbp.go
+
+  rbp.mappings <- get_all_mappings(
+    go_ids = rbp.go,
+    ontologies = rbp.ontologies,
+    verbose = TRUE,
+    direction = "offspring"
+  )
+
+  expect_equal(length(rbp.mappings), 2)
+  expect_equal(length(rbp.mappings[[1]]), 173)
+  expect_equal(length(rbp.mappings[[2]]), 4)
+})
+
+test_that("expand_terms works", {
+  rbp.expanded <- expand_terms(go_df = go.df, go_col = "GO.ID", go2Ancestor = rbp.mappings)
+
+  expect_equal(rbp.expanded$GOID, unique(c("GO:0003723", rbp.mappings[[1]], rbp.mappings[[2]])))
+})
+
 test_that("get_ancestor_go works", {
   ancestor.terms <- get_ancestor_go(go.df, verbose = FALSE)
 
