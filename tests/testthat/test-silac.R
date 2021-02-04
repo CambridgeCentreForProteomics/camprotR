c9c67621c3d2eb5b0f945574fb004cd6480d3b83
Charlotte Dawson <csdaw@outlook.com>
2021-02-04 11:37:53 +0000

    Update test-silac.R descriptions

--- tests/testthat/test-silac.R
+++ tests/testthat/test-silac.R
@@ -1,17 +1,22 @@
-context('SILAC incorporation')
+context('silac')
 
-test_that("No heavy", {
+#### Setup ---------------------------------------------------------------------
+
+#### Tests ---------------------------------------------------------------------
+test_that("No heavy produces ratio = 0", {
   expect_equal(get_incorporation(light = 1, heavy = NA), 0)
 })
 
-test_that("No light", {
+test_that("No light produces ratio = 1", {
   expect_equal(get_incorporation(light = NA, heavy = 1), 1)
 })
 
-test_that("No heavy or light", {
+test_that("No heavy or light produces ratio = NA", {
   expect_equal(get_incorporation(light = NA, heavy = NA), NA)
 })
 
-test_that("Both heavy and light", {
+test_that("Both heavy and light produces the correct ratio", {
   expect_equal(get_incorporation(light = 1, heavy = 2), 2/3)
 })
+
+#### Sanity checks -------------------------------------------------------------
