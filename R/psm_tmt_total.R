697fcd29e87bd45236b7e9bd0c0fd33b11e4a8be
Charlotte Dawson <csdaw@outlook.com>
2021-02-16 15:44:10 +0000

    Document TMT total proteome PSM data
    Also:
    - provide R script to regenerate .rda file when
      necessary
    - delete the .txt file from extdata
    - update vignette with new .rda file

---
+++ R/psm_tmt_total.R
@@ -0,0 +1,12 @@
+#' PSM-level PD output for TMT10 plex data
+#'
+#' @description Description.
+#'
+#' \describe{
+#'   \item{column name}{`class`. Description.}
+#' }
+#'
+#' @references Insert reference here when published.
+#'
+#' @keywords datasets
+"psm_tmt_total"
