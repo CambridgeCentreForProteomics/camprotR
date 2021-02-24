2dc0239364b07db38eb8d80bfda1abb0f5b956cb
Charlotte Dawson <csdaw@outlook.com>
2021-02-24 16:28:44 +0000

    Test get_parsimony_pep2prot

---
+++ data-raw/small_pep_silac2.R
@@ -0,0 +1,26 @@
+# Read in SILAC peptide level data
+silac <- read.delim(
+  here::here("data-raw/H2_PeptideGroups.txt")
+)
+
+# Set seed for reproducible sampling
+set.seed(2021)
+
+# Randomly sample 1000 rows
+silac_filt <- silac[sample(nrow(silac), 1000), ]
+
+# Select the columns we need
+output <- silac_filt %>%
+  select(
+    Sequence,
+    Modifications,
+    Master.Protein.Accessions,
+    Protein.Accessions,
+    Number.of.Protein.Groups,
+    Quan.Info,
+    Abundances.Grouped.Light,
+    Abundances.Grouped.Heavy
+  )
+
+# Output text file
+readr::write_delim(output, "inst/testdata/small_pep_silac2.txt", delim = "\t")
--- tests/testthat/test-combine_runs.R
+++ tests/testthat/test-combine_runs.R
@@ -2,7 +2,17 @@ context('combine_runs')
 
 #### Setup ---------------------------------------------------------------------
 
+
 #### Tests ---------------------------------------------------------------------
+test_that("get_parsimony_pep2prot works on some SILAC peptide data", {
+  expect_equal_to_reference(
+    get_parsimony_pep2prot(list(
+      system.file("testdata", "small_pep_silac.txt", package = "camprotR"),
+      system.file("testdata", "small_pep_silac2.txt", package = "camprotR"))),
+    "reference/get_parsimony_pep2prot.rds"
+  )
+})
+
 test_that("single protein, matched", {
   expect_true(camprotR:::match_proteins('prot1', 'prot1'))
 })
