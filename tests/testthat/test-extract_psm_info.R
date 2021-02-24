ae9233ce93e6f1a7dd6cf64b35d483f1e0e588c5
Charlotte Dawson <csdaw@outlook.com>
2021-02-24 16:46:14 +0000

    Replace SILAC PSM test data

--- tests/testthat/test-extract_psm_info.R
+++ tests/testthat/test-extract_psm_info.R
@@ -2,18 +2,14 @@ context('extract_psm_info')
 
 #### Setup ---------------------------------------------------------------------
 
-#### Tests ---------------------------------------------------------------------
-test_that("Extracting sequenced and interference data from PSM", {
-  data('silac_psm_data', package='camprotR')
-  out <-  silac_psm_seq_int(silac_psm_data, sequence_col='Annotated.Sequence') %>%
-    dplyr::arrange(Annotated.Sequence, Modifications)
+load(system.file("testdata", "small_psm_silac.rda", package = "camprotR"))
 
-  #expect_equal_to_reference(
-  #  dplyr::arrange(output, Annotated.Sequence, Modifications), # sort order appears to be env-specific
-  #  file='psm_sequenced.rds')
-  ref <- readRDS('reference/psm_sequenced.rds') %>%
-    dplyr::arrange(Annotated.Sequence, Modifications)
-  expect_equal(out, ref)
+#### Tests ---------------------------------------------------------------------
+test_that("silac_psm_seq_int works", {
+  expect_equal_to_reference(
+    silac_psm_seq_int(small_psm_silac, sequence_col='Annotated.Sequence'),
+    "reference/silac_psm_seq_int.rds"
+  )
 })
 
 #### Sanity checks -------------------------------------------------------------
