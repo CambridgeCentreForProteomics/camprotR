d14667d93966a53b08965751d6623328025aa8df
Charlotte Dawson <csdaw@outlook.com>
2021-03-08 17:26:09 +0000

    Clean up tests after merge

--- tests/testthat/test-center-normalise.R
+++
@@ -1,7 +0,0 @@
-context('Center-normalise')
-
-test_that("Center-normalise, no reference", {
-  data('tmt_psm', package='camprotR')
-  expect_equal_to_reference(center_normalise_to_ref(tmt_psm, get_medians(tmt_psm)),
-                            file='tmt_center_normalised.rds')
-})
