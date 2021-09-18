33fd3ee9fca2f517155bbbb4c0e2373f3375a7dd
Charlotte Dawson <csdaw@outlook.com>
2021-09-18 08:10:50 +0100

    Avoid using internet in append_fasta() tests

--- tests/testthat/test-append_fasta.R
+++ tests/testthat/test-append_fasta.R
@@ -1,30 +1,32 @@
 test_that("append_fasta() works", {
-  withr::with_tempfile(c("tf1", "tf2"), {
-    # save FUBP3 fasta to tempfile 1
-    httr::GET("https://www.uniprot.org/uniprot/Q96I24.fasta", config = httr::write_disk(path = tf1))
+  # create self cleaning tempfile
+  withr::with_tempfile("tf", {
+    # make some fake fasta
+    Biostrings::writeXStringSet(
+      x = Biostrings::AAStringSet(x = c("a fasta header" = "QQQPPPMMM")),
+      filepath = tf
+    )
 
-    # save GAPDH fasta to tempfile 2
-    httr::GET("https://www.uniprot.org/uniprot/P04406.fasta", config = httr::write_disk(path = tf2))
-
-    # append FUBP3 fasta to GAPDH fasta file
+    # append 'commercial_reagents' fasta to fake fasta file
     append_fasta(
-      file1 = tf1,
-      file2 = tf2,
+      file1 = system.file("extdata/commercial_reagents.fasta", package = "camprotR"),
+      file2 = tf,
       is_crap = TRUE,
       crap_start = 1
     )
 
-    # read in tempfile2 which should have been modified by append_fasta
-    target <- Biostrings::readAAStringSet(tf2)
+    # read in tempfile which should have been modified by append_fasta()
+    target <- Biostrings::readAAStringSet(tf)
 
     # check fasta lengths are correct
-    expect_equal(lengths(target), c(335, 572))
+    expect_equal(lengths(target), c(9, 274, 261))
 
     # check fasta headers are okay
-    expect_true(grepl("P04406", names(target)[1]))
-    expect_true(grepl("Q96I24", names(target)[2]))
+    expect_true(grepl("Endoproteinase GluC", names(target)[2]))
+    expect_true(grepl("rLys-C", names(target)[3]))
 
     # check CRAP number has been added
     expect_true(grepl("cRAP001", names(target)[2]))
+    expect_true(grepl("cRAP002", names(target)[3]))
   })
 })
