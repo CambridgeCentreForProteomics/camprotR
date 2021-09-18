40f8d078d0e4fb9dcbd4e6be7d046bae5dd14960
Charlotte Dawson <csdaw@outlook.com>
2021-09-18 10:02:20 +0100

    Transfer small test MSnSets to tests/
    Instead of installing these test MSnSets with the
    package, just generate them at the start of testing
    and get rid of them when finished.
    
    Thereby removing unnecessary data files from package
    and reducing package size.

--- tests/testthat/test-setup.R
+++ tests/testthat/test-setup.R
@@ -1,4 +1,10 @@
 test_that("self cleaning test infrastructure works", {
   df <- read.delim(file = test_path("testdata/letters.txt"), header = TRUE)
   expect_equal(df$letters, c("A", "B", "C"))
+
+  load(test_path("testdata/small_psm_tmt_phospho.rda"))
+  expect_equal(dim(small_psm_tmt_phospho), c(234, 10))
+
+  load(test_path("testdata/small_psm_tmt_total.rda"))
+  expect_equal(dim(small_psm_tmt_total), c(100, 10))
 })
