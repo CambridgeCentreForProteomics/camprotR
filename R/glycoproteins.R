c2496d0a134a61ff018feea0f9932dfef34fee19
TomSmithCGAT <tss38@mrc-tox.cam.ac.uk>
2022-03-08 14:09:17 +0000

    finalises LFQ vignette to protein inference

---
+++ R/glycoproteins.R
@@ -0,0 +1,11 @@
+#' Glycoproteoins
+#'
+#' @description data.fram with glycoproteins for use in LFQ vignette
+#'
+#' \describe{
+#'   \item{column name}{`class`. Description.}
+#' }
+#'
+#'
+#' @keywords datasets
+"glycoproteins"
