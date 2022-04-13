c2ffa83ab93ed35cbf702833fbffc788bf02a65d
Tom Smith <tss38@cam.ac.uk>
2022-04-13 16:33:10 +0100

    Ts add lfq vignette (#42)
    * adds LFQ vignette
    
    * finalises LFQ vignette to protein inference
    
    * updates normalise to ref
    
    * finalises first draft LFQ vignette
    
    * resolve dependencies
    
    * resolve dependencies(2)
    
    * clarifies missing heatmap colours
    
    * Query UniProt directly in LFQ vignette
    
    UniProt.ws is not great. Now we just query the
    UniProt REST API directly using the `httr` package.
    
    Also other small changes to MSnSet construction
    and other code in LFQ vignette.
    
    * iq is back on CRAN: https://github.com/tvpham/iq/issues/2
    
    * Finish CSD review of LFQ vignette
    
    * Put back filter using pData
    
    * Resolve some R CMD check notes
    
    Co-authored-by: TomSmithCGAT <tss38@mrc-tox.cam.ac.uk>
    Co-authored-by: Charlotte Dawson <csdaw@outlook.com>

---
+++ R/rbps.R
@@ -0,0 +1,11 @@
+#' RNA-binding proteins
+#'
+#' @description List of RNA binding proteins for use in LFQ vignette
+#'
+#' \describe{
+#'   \item{column name}{`class`. Description.}
+#' }
+#'
+#'
+#' @keywords datasets
+"rbps"
