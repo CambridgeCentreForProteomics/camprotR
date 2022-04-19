88c502a61a2ca1ec290eb53b0d492b9cda973db6
TomSmithCGAT <tss38@mrc-tox.cam.ac.uk>
2022-04-19 15:11:34 +0100

    updates LFQ vignette for reduced input

--- data-raw/lfq_oops_rnase.R
+++ data-raw/lfq_oops_rnase.R
@@ -7,12 +7,19 @@ lfq_oops_rnase_pep <- read.delim(
   here::here("data-raw/RBPMap_RNase_pilot_3/LFQ_OOPS_RNase_MetChloroform-(1)_PeptideGroups.txt")
 )
 
+random_proteins <- sample(unique(lfq_oops_rnase_pep$Master.Protein.Accessions), 100)
+lfq_oops_rnase_pep <- lfq_oops_rnase_pep %>% filter(Master.Protein.Accessions %in% random_proteins)
+
+
 # Output .rda file
-usethis::use_data(lfq_oops_rnase_pep)
+usethis::use_data(lfq_oops_rnase_pep, overwrite=TRUE)
 
 lfq_oops_rnase_psm <- read.delim(
   here::here("data-raw/RBPMap_RNase_pilot_3/LFQ_OOPS_RNase_MetChloroform-(1)_PSMs.txt")
 )
 
+
+lfq_oops_rnase_psm <- lfq_oops_rnase_psm %>% filter(Master.Protein.Accessions %in% random_proteins)
+
 # Output .rda file
-usethis::use_data(lfq_oops_rnase_psm)
+usethis::use_data(lfq_oops_rnase_psm, overwrite=TRUE)
