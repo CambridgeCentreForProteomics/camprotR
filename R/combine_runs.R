8e9a8418e44d7cc4a5958a0470f0b6ac05210b50
Charlotte Dawson <csdaw@outlook.com>
2022-04-26 15:04:45 +0100

    Update `combine_runs.R` docs

--- R/combine_runs.R
+++ R/combine_runs.R
@@ -1,55 +1,66 @@
-#' Extract sequence:protein ID mappings
+#' Extract peptide sequence:protein ID mappings
 #'
-#' @description Given a list of infiles, read the files,
-#' retain the sequence and protein ID columns and bind the rows
+#' @description Given a list of `infiles`, read the files into a list of
+#' data.frames, retain the peptide sequence and protein ID columns, then
+#' combine the data.frames into one by binding the rows.
 #'
-#' @param infiles `list`. Outfiles from PD
-#' @param seq_col `string` Column name for peptide sequence
-#' @param prot_col `string` Column name for protein(s)
-#' @param master_prot_col `string` Column name for master protein(s)
-#' @return Returns an `data.frame` object which maps peptide sequences to
-#' protein and master protein ID(s) for each infile
+#' @param infiles `list`, paths for PD peptideGroups.txt files to be read
+#' by \code{\link[utils]{read.delim}}.
+#' @param seq_col `string`, column name for peptide sequence e.g. `"Sequence"`.
+#' @param prot_col `string`, column name for protein accessions e.g.
+#' `"Protein.Accessions"`.
+#' @param master_prot_col `string`, column name for master protein accessions
+#' e.g. `"Master.Protein.Accessions"`.
+#'
+#' @return Returns a `data.frame` object which maps peptide sequences to
+#' protein and master protein ID(s) for each infile. Each row is a peptide.
 #' @keywords internal
 read_infiles <- function(infiles,
-                        seq_col,
-                        prot_col,
-                        master_prot_col){
-  seq2prot <- lapply(infiles, function(x){
-    utils::read.delim(x, stringsAsFactors=FALSE) %>%
+                         seq_col,
+                         prot_col,
+                         master_prot_col) {
+  seq2prot <- lapply(infiles, function(x) {
+    utils::read.delim(x, stringsAsFactors = FALSE) %>%
       dplyr::select(all_of(c(seq_col, prot_col, master_prot_col))) %>%
-      mutate(infile=x)
-  }) %>% do.call(what='rbind')
+      mutate(infile = x)
+  }
+  ) %>%
+    do.call(what = 'rbind')
 
   return(seq2prot)
 }
 
-#' Approximate parsimony approach to map sequence to protein
-#' across multiple samples
+#' Map peptide sequence to protein across multiple files using
+#' approximate parsimony
 #'
-#' @description # Uses an approximate parsimony approach (e.g least number of
-#' proteins required to account for all observed sequences) to generate a
-#' single map from sequence to protein for all samples
+#' @description Generate a single map of peptide sequences to proteins for
+#' multiple files (e.g. multiple separate peptideGroups.txt PD files) using
+#' an approximate parsimony approach, specifically the least number
+#' of proteins required to account for all observed peptide sequences.
 #'
-#' @param seq2prot `data`. Output from \code{\link{read_infiles}}
-#' @param seq_col `string` Column name for peptide sequence
-#' @param prot_col `string` Column name for protein(s)
-#' @param master_prot_col `string` Column name for master protein(s)
-#' @return Returns an `data.frame` object which uniquely maps peptide sequence
-#' to master protein
+#' @param seq2prot `data.frame`, output from \code{\link{read_infiles}}.
+#' @param seq_col `string`, column name for peptide sequence e.g. `"Sequence"`.
+#' @param prot_col `string`, column name for protein accessions e.g.
+#' `"Protein.Accessions"`.
+#' @param master_prot_col `string`, column name for master protein accessions
+#' e.g. `"Master.Protein.Accessions"`.
+#'
+#' @return Returns a `data.frame` object which uniquely maps peptide sequence
+#' to master protein(s).
 #' @keywords internal
 get_sequence2protein <- function(seq2prot,
                                  seq_col,
-                                 prot_col){
+                                 prot_col) {
 
   # get unique seq->prot mappings
   seq_protein <- seq2prot %>%
-    separate_rows(.data$Protein.Accessions, sep='; ') %>%
+    separate_rows(.data[[prot_col]], sep='; ') %>%
     dplyr::select(all_of(c(seq_col, prot_col))) %>%
     distinct()
 
   protein_to_sequences <- seq_protein %>%
     group_by_at(prot_col) %>%
-    summarise(Sequences=list(!!(sym(seq_col))))
+    summarise(Sequences = list(!!(sym(seq_col))))
 
   protein_to_sequences_map <- protein_to_sequences$Sequences
 
@@ -66,19 +77,19 @@ get_sequence2protein <- function(seq2prot,
   s2p <- NULL
   for(n in n_seq_per_protein_values){
 
-    if(length(sequences_left_to_account_for)==0){
+    if(length(sequences_left_to_account_for) == 0){
       break()
     }
 
-    proteins <- protein_counts[protein_counts==n]
+    proteins <- protein_counts[protein_counts == n]
 
-    mini_s2p <- rep(names(proteins), each=n)
-    names(mini_s2p) <- unlist(protein_to_sequences_map[names(proteins)], use.names=FALSE)
+    mini_s2p <- rep(names(proteins), each = n)
+    names(mini_s2p) <- unlist(protein_to_sequences_map[names(proteins)], use.names = FALSE)
     mini_s2p <- mini_s2p[!names(mini_s2p) %in% sequences_accounted_for]
 
     rep_seq <- intersect(names(mini_s2p), sequences_accounted_for)
 
-    if(length(rep_seq>0)){
+    if(length(rep_seq > 0)){
       stop(sprintf('Something has gone wrong here, accounting for the same sequence more than once %s',
                    paste(rep_seq, collapse=',')))
     }
@@ -90,31 +101,31 @@ get_sequence2protein <- function(seq2prot,
   }
 
   new_seq_to_master <-
-    rlang::set_names(data.frame(names(s2p), s2p), c(seq_col, 'Protein')) %>%
-    group_by_at(seq_col) %>%
-    summarise('Updated.Master.Protein.Accessions'=paste(.data$Protein, collapse='; '))
+    `colnames<-`(data.frame(names(s2p), s2p), c(seq_col, 'Protein')) %>%
+    group_by(across(seq_col)) %>%
+    summarise('Updated.Master.Protein.Accessions' = paste(.data$Protein, collapse='; '))
 
   new_seq_to_master
 }
 
-#' Check if two protein IDs are the same allowing for:
-#' 1. multiple IDs (default separated by '; ')
-#' 2. mutiple IDs in any order
+#' Compare two protein accession strings
 #'
-#' @description Check if two protein IDs are the same allowing for:
-# 1. multiple IDs (default separated by '; ')
-# 2. multiple IDs in any order
+#' @description Check if two protein accession strings are the same,
+#' allowing for:
 #'
-#' @param seq2prot `data`. Output from \code{\link{read_infiles}}
-#' @param proteins1 `string` protein(s), set 1
-#' @param proteins2 `string` protein(s), set 2
-#' @param sep1 `string` delimiter for proteins1
-#' @param sep2 `string` delimiter for proteins2
-#' @return Returns an `Logical` where `TRUE` if the proteins1 and
-#' proteins2 contain the same set of proteins
+#' 1. multiple accessions (default separated by '; ')
+#' 1. multiple accessions in any order
+#'
+#' @param proteins1 `string`, protein acession(s), set 1
+#' @param proteins2 `string`, protein accession(s), set 2
+#' @param sep1 `string`, delimiter for proteins1. Default is `'; '`.
+#' @param sep2 `string`, delimiter for proteins2. Default is `'; '`.
+#'
+#' @return Returns `TRUE` if the proteins1 and proteins2 contain the same set
+#' of protein accessions.
 #' @keywords internal
-match_proteins <- function(proteins1, proteins2, sep1='; ', sep2='; '){
-  if(proteins1==proteins2){
+compare_proteins <- function(proteins1, proteins2, sep1='; ', sep2='; ') {
+  if(proteins1 == proteins2){
     return(TRUE)
   }
 
@@ -124,70 +135,78 @@ match_proteins <- function(proteins1, proteins2, sep1='; ', sep2='; '){
   return(setequal(proteins1, proteins2))
 }
 
-#' Compare the number of peptides per protein pre/post-consensus master protein
+#' Plot the number of peptides per protein pre/post-consensus master protein
 #' assignment
 #'
-#' @param seq2new_master_prot `data`. Output from merging
-#' \code{\link{read_infiles}} & \code{\link{get_sequence2protein}}
-#' @param seq_col `string` Column name for peptide sequence
-#' @param master_prot_col `string` Column name for master protein(s)
-#' @return Returns an `ggplot` for the number of sequences per master protein
-#' in the original infiles and after the consensus master protein assignment
+#' @param seq2new_master_prot `data.frame`, output of merging output of
+#' \code{\link{read_infiles}} & \code{\link{get_sequence2protein}}.
+#' @param seq_col `string`, column name for peptide sequence e.g. `"Sequence"`.
+#' @param master_prot_col `string`, column name for master protein accessions
+#' e.g. `"Master.Protein.Accessions"`.
+#'
+#' @return Returns a `ggplot` showing the number of sequences per master protein
+#' in the original infiles, and after the consensus master protein assignment.
 #' @keywords internal
-compare_sequences_per_protein <- function(seq2new_master_prot,
-                                          seq_col,
-                                          master_prot_col){
+plot_sequences_per_protein <- function(seq2new_master_prot,
+                                       seq_col,
+                                       master_prot_col) {
 
   master_protein_counts <- seq2new_master_prot %>%
-    filter(!!sym(master_prot_col)!='') %>%
+    filter(!!sym(master_prot_col) != '') %>%
     dplyr::select(one_of(c(seq_col, master_prot_col))) %>%
     distinct() %>%
-    group_by_at(master_prot_col) %>%
+    group_by(across(master_prot_col)) %>%
     tally() %>%
     group_by(n) %>%
     tally() %>%
-    mutate(type='Original')
+    mutate(type = 'Original')
 
   new_master_protein_counts <- seq2new_master_prot %>%
-    filter(.data$Updated.Master.Protein.Accessions!='') %>%
+    filter(.data$Updated.Master.Protein.Accessions != '') %>%
     dplyr::select(one_of(c(seq_col, 'Updated.Master.Protein.Accessions'))) %>%
     distinct() %>%
     group_by(.data$Updated.Master.Protein.Accessions) %>%
     tally() %>%
     group_by(n) %>%
     tally() %>%
-    mutate(type='Updated')
+    mutate(type = 'Updated')
 
-  p <- new_master_protein_counts %>% rbind(master_protein_counts) %>%
+  p <- new_master_protein_counts %>%
+    rbind(master_protein_counts) %>%
     arrange(.data$type) %>%
-    mutate(type=factor(.data$type, levels=c('Original', 'Updated'))) %>%
+    mutate(type = factor(.data$type, levels = c('Original', 'Updated'))) %>%
     group_by(.data$type) %>%
-    mutate(cum_nn =cumsum(.data$nn)/sum(.data$nn)) %>%
-    ggplot(aes(log10(n), .data$cum_nn, colour=.data$type)) +
+    mutate(cum_nn = cumsum(.data$nn) / sum(.data$nn)) %>%
+    ggplot(aes(x = log10(n), y = .data$cum_nn, colour = .data$type)) +
     geom_step() +
     theme_bw() +
-    theme(aspect.ratio=1) +
-    scale_colour_discrete(name='Master protein assignment') +
+    theme(aspect.ratio = 1) +
+    scale_colour_discrete(name = 'Master protein assignment') +
     ylim(0, NA) +
-    xlab('Sequences per protein (log10)') +
-    ylab('Fraction of proteins')
+    labs(
+      x = 'Sequences per protein (log10)',
+      y = 'Fraction of proteins'
+    )
 
   invisible(p)
 }
 
-#' Identify the proportion of exact matches in protein IDs
+#' Identify the proportion of exact matches in protein accessions
 #'
-#' @description With object containing original and updated master protein
-#' assignments, identify the proportion of exact matches in protein IDs
-#' @param seq2new_master_prot `data`. Output from merging
-#' \code{\link{read_infiles}} & \code{\link{get_sequence2protein}}
-#' @param master_prot_col `string` Column name for master protein(s)
+#' @description Identify the proportion of exactly matching protein accessions
+#' in a data.frame containing original and updated master protein assignments.
+#'
+#' @param seq2new_master_prot `data.frame`, output of merging
+#' \code{\link{read_infiles}} & \code{\link{get_sequence2protein}}.
+#' @param master_prot_col `string`, column name for master protein accessions
+#' e.g. `"Master.Protein.Accessions"`.
+#'
 #' @return Prints the proportion of exact matches in the console.
 #' @keywords internal
-compare_IDs <- function(seq2new_master_prot, master_prot_col){
+compare_sequences_per_protein <- function(seq2new_master_prot, master_prot_col){
   id_match <- seq2new_master_prot %>%
     rowwise() %>%
-    mutate(same_id=ifelse(match_proteins(
+    mutate(same_id=ifelse(compare_proteins(
       !!sym(master_prot_col), .data$Updated.Master.Protein.Accessions),
       'Same ID(s)', 'Different ID(s)')) %>%
     pull(.data$same_id) %>%
@@ -203,46 +222,61 @@ compare_IDs <- function(seq2new_master_prot, master_prot_col){
 
 #' Identify the proportion of peptides with a single assigned master protein
 #'
-#' @description With object containing original and updated master protein
-#' assignments, identify the proportion with a single assigned master protein
-#' @param seq2new_master_prot `data`. Output from merging
-#' \code{\link{read_infiles}} & \code{\link{get_sequence2protein}}
-#' @param master_prot_col `string` Column name for master protein(s)
-#' @return Returns an `ggplot` for the number of sequences per master protein
-#' in the original infiles and after the consensus master protein assignment
+#' @description Identify the proportion of peptides with a single assigned
+#' master protein in a data.frame containing original and updated master protein
+#' assignments.
+#'
+#' @param seq2new_master_prot `data.frame`, output of merging
+#' \code{\link{read_infiles}} & \code{\link{get_sequence2protein}}.
+#' @param master_prot_col `string`, column name for master protein accessions
+#' e.g. `"Master.Protein.Accessions"`.
+#'
+#' @return Returns a `ggplot` showing the number of peptide sequences per
+#' master protein  in the original infiles, and after the consensus master
+#' protein assignment.
 #' @keywords internal
-compare_single_master <- function(seq2new_master_prot, master_prot_col){
+compare_single_master <- function(seq2new_master_prot,
+                                  master_prot_col) {
 
   single_master <- seq2new_master_prot %>%
-    mutate(original=ifelse(grepl('; ', !!(sym(master_prot_col))),
-                                 'Original:Multiple', 'Original:Single'),
-           updated=ifelse(grepl('; ', .data$Updated.Master.Protein.Accessions),
-                          'Updated:Multiple', 'Updated:Single')) %>%
+    mutate(original = ifelse(grepl('; ', !!(sym(master_prot_col))),
+                             'Original:Multiple',
+                             'Original:Single'),
+           updated = ifelse(grepl('; ', .data$Updated.Master.Protein.Accessions),
+                            'Updated:Multiple',
+                            'Updated:Single')) %>%
     group_by(.data$original, .data$updated) %>%
     count() %>%
-    spread(key=.data$updated, value=n) %>%
+    spread(key = .data$updated, value = n) %>%
     tibble::column_to_rownames('original')
 
   print(single_master)
-  print(round(100*single_master/sum(single_master), 2))
+  print(round(100*single_master / sum(single_master), 2))
 
   invisible(NULL)
 }
 
-#' Identify the consensus peptide to master protein mapping across multiple runs
+#' Get consensus peptide to master protein mapping across multiple files
 #'
-#' @description Wrapper function to read in infiles,
-#' identify the parsimonious proteins to account for the peptides and
-#' then (optionally) compare the new and old master protein assignments
+#' @description Given multiple peptideGroups.txt files, identify the
+#' parsimonious master proteins to account for the all the peptides identified
+#' and (optionally) compare the new and old master protein assignments.
 #'
-#' @param infiles `list`. Outfiles from PD
-#' @param seq_col `string` Column name for peptide sequence
-#' @param prot_col `string` Column name for protein(s)
-#' @param master_prot_col `string` Column name for master protein(s)
-#' @param compare_old_new `logical` Compare the consensus master protein
-#' assignments with the original assignments
-#' @return Returns an `data.frame` object which maps peptide sequences to
-#' consensus master protein ID(s)
+#' The output of this function can then be used when merging multiple
+#' peptideGroups.txt files (i.e. the outputs of separate PD studies).
+#'
+#' @param infiles `list`, paths for PD peptideGroups.txt files to be read
+#' by \code{\link[utils]{read.delim}}.
+#' @param seq_col `string`, column name for peptide sequence e.g. `"Sequence"`.
+#' @param prot_col `string`, column name for protein accessions e.g.
+#' `"Protein.Accessions"`.
+#' @param master_prot_col `string`, column name for master protein accessions
+#' e.g. `"Master.Protein.Accessions"`.
+#' @param compare_old_new `logical`, compare the consensus master protein
+#' assignments with the original assignments. Default is `TRUE`.
+#'
+#' @return Returns a `data.frame` which maps peptide sequences to
+#' consensus master protein accession(s). Each row is a peptide.
 #' @examples
 #' \dontrun{
 #' # The input in this example is 2x peptideGroups.txt outputs from Proteome
@@ -259,29 +293,30 @@ compare_single_master <- function(seq2new_master_prot, master_prot_col){
 #'
 #' }
 #' @export
-get_parsimony_pep2prot <- function(infiles, seq_col='Sequence',
-                                  prot_col='Protein.Accessions',
-                                  master_prot_col='Master.Protein.Accessions',
-                                  compare_old_new=TRUE){
+get_parsimony_pep2prot <- function(infiles,
+                                   seq_col = 'Sequence',
+                                   prot_col = 'Protein.Accessions',
+                                   master_prot_col = 'Master.Protein.Accessions',
+                                   compare_old_new = TRUE) {
   seq2prot <- read_infiles(infiles, seq_col, prot_col, master_prot_col)
 
   new_seq_to_master <- get_sequence2protein(seq2prot, seq_col, prot_col)
 
-  seq2new_master_prot <- seq2prot %>% merge(new_seq_to_master, by=seq_col)
+  seq2new_master_prot <- merge(seq2prot, new_seq_to_master, by=seq_col)
 
-  if(compare_old_new){
+  if(compare_old_new) {
 
     message(
       sprintf(
         'With original assignments, %s master proteins. With update, %s master proteins\n',
         length(unique(seq2new_master_prot[[master_prot_col]])),
-        length(unique(seq2new_master_prot$Updated.Master.Protein.Accessions))))
+        length(unique(seq2new_master_prot$Updated.Master.Protein.Accessions)))
+    )
 
-    print(compare_sequences_per_protein(
-      seq2new_master_prot, seq_col, master_prot_col))
+    print(plot_sequences_per_protein(seq2new_master_prot, seq_col, master_prot_col))
 
     message('Comparing Master Protein IDs\n')
-    compare_IDs(seq2new_master_prot, master_prot_col)
+    compare_sequences_per_protein(seq2new_master_prot, master_prot_col)
     compare_single_master(seq2new_master_prot, master_prot_col)
   }
 
